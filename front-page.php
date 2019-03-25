701d8a910408dbc3f7d0046543a6fdba6d60f33e
Dantolos <aaron.giaimo@gmail.com>
2019-03-25 13:42:47 +0100

    1.0.32
    - Slide show Zitat anpassung

--- front-page.php
+++ front-page.php
@@ -65,8 +65,10 @@ endif;
 <div id="slider" class="se-slider-header-container image-settings se-slider-mobile-align-left" style="background-image:url('<?php echo $slideArray[0]['image'] ?>');">
   <div class="se-slider-header-text-container">
     <?php if(!wp_is_mobile()) {
-      $displayer = ($slideArray[0]['zitat']) ? 'inline' : 'none'; ?>
-    <p class="se-slider-header-zitat" style="display:<?php echo $displayer; ?>;"><strong>&laquo;</strong><?php echo $slideArray[0]['zitat']; ?><strong>&raquo;</strong></p>
+      $displayer = ($slideArray[0]['zitat']) ? 'block' : 'none'; ?>
+      <div class="se-slider-header-zitat">
+        <p class="" style="display:<?php echo $displayer; ?>;"><strong>&laquo;</strong><?php echo $slideArray[0]['zitat']; ?><strong>&raquo;</strong></p>
+      </div>
     <?php } ?>
 
     <p id="slidename" style="margin-top:20px; color:<?php echo $slideArray[0]['color']; ?>;"><strong><?php echo $slideArray[0]['name']; ?></strong></p>
@@ -144,6 +146,7 @@ if( get_field('strip', $curPageID)  ) {
 
         slidesTC.animate({ }, function() {
             slideZitat.empty();
+            slideZitat.fadeOut();
             slideName.empty();
             slideFunktion.empty();
             slideButton.empty();
@@ -157,12 +160,12 @@ if( get_field('strip', $curPageID)  ) {
 
         setTimeout(function() {
             var offsetRight = (!isMobile) ? '20vw' : '5vw';
-            var displayer = (SlideArray[currentSlide]['zitat']) ? 'inline' : 'none';
+            var displayer = (SlideArray[currentSlide]['zitat']) ? 'block' : 'none';
             slidesTC.css({'right': '-500', 'opacity': '0'}).animate({'right': offsetRight, 'opacity': '1' },700);
             slideZitat.css({'display': displayer})
             slideName.css({'color': SlideArray[currentSlide]['color']});
             slideFunktion.css({'color': SlideArray[currentSlide]['color']});
-            slideZitat.append('<strong>&laquo;</strong>'+SlideArray[currentSlide]['zitat']+'<strong>&raquo;</strong>');
+            slideZitat.append('<p><strong>&laquo;</strong>'+SlideArray[currentSlide]['zitat']+'<strong>&raquo;</strong></p>');
             slideName.append('<strong>'+SlideArray[currentSlide]['name']+'</strong>');
             slideFunktion.append(SlideArray[currentSlide]['funktion']);
             if ( SlideArray[currentSlide]['button'] === true ) {

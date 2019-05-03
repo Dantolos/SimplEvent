af8b6b2d80b7d5d7ad45de3341f3b6a17f82b39e
Dantolos <aaron.giaimo@gmail.com>
2019-05-03 09:37:03 +0200

    1.0.49
    - Review Video
    - Review Media in Icon
    - Review Fixes Mobile

--- template-review.php
+++ template-review.php
@@ -7,6 +7,28 @@ get_header();
 
 $Olink = new LinkIcon;
 $rCount = 0;
+
+//Button Medien in Variable
+$RevMediaButton = '<div class="se-review-col" style="text-align:center;">';
+$RevMediaButton .= '<div class="button-border se-review-button se-review-medien-button" mid="' . $rCount . '"  mopen="N">';
+$RevMediaButton .= '<a>';
+$RevMediaButton .= '<svg version="1.1" id="Medien" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="50%" y="50%"
+   viewBox="0 0 148.7 148.7" style="enable-background:new 0 0 148.7 148.7;" xml:space="preserve">';
+
+
+$RevMediaButton .= '<style type="text/css"> .icon-in{fill:#C3B940;} </style>';
+$RevMediaButton .= '<g>
+                      <path class="icon-in" d="M91.4,72.9H88c9.2-4.6,15.6-14.2,15.6-25.1c0-15.5-12.6-28.1-28.1-28.1S47.3,32.2,47.3,47.7
+                        c0,11,6.3,20.5,15.6,25.1h-5.6c-0.2,0-21.9,0.3-21.9,21.9v34.1h2.7V94.8c0-18.9,18.4-19.2,19.2-19.2h34.1
+                        c18.9,0,19.2,18.4,19.2,19.2v34.1h2.7V94.8C113.3,94.6,113,72.9,91.4,72.9z M50,47.7c0-14,11.4-25.4,25.4-25.4
+                        c14,0,25.4,11.4,25.4,25.4c0,12.8-9.5,23.4-21.7,25.1h-7.4C59.5,71.1,50,60.5,50,47.7z"/>
+                    </g>';
+$RevMediaButton .= '</svg></a></div>';
+$RevMediaButton .= '<h5>' . __('MEDIEN', 'SimplEvent') . '</h5>';
+$RevMediaButton .= '<h6>'. __('Schauen Sie alle nochmals alle Referate in Ruhe durch', 'SimplEvent') . '</h6>';
+$RevMediaButton .= '</div>';
+
+
 if( have_rows('reviews') ) {
   while ( have_rows('reviews') ) : the_row();
     $thumb = get_sub_field('thumb');
@@ -17,9 +39,14 @@ if( have_rows('reviews') ) {
   ?>
 
     <div class="se-strip se-review-strip" style="padding: 0; position:relative;" rid="<?php echo $rCount; ?>" >
-      <div class="se-review-bg image-settings" style="background-image:url(<?php echo $thumb['url']; ?>);">
+      <?php
+        if( get_sub_field('report') ){
+          echo '<div class="se-review-bg"><iframe width="100%" height="100%" src="' . get_sub_field('report') . '" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>';
+        } else {
+          echo '<div class="se-review-bg image-settings" style="background-image:url( ' . $thumb['url'] . ' );"></div>';
+        }
+      ?>
 
-      </div>
       <div class="se-sc-bg se-wc-txt se-review-content" style="border-left:2px solid <?php echo esc_attr( get_option( 'main_color_picker' ) ) ; ?>;">
         <div class="se-review-content-title" style="position:relative;" rid="<?php echo $rCount; ?>" copen="N">
           <h5><?php echo get_sub_field('motto'); ?></h5>
@@ -29,6 +56,41 @@ if( have_rows('reviews') ) {
         <div class="se-review-content-inner clearfix" style="display:none;" rid="<?php echo $rCount; ?>">
 
           <p><?php echo get_sub_field('zusammenfassung'); ?></p>
+
+
+          <!-- Speakers -->
+          <div class="se-review-col" style="text-align:center;">
+            <a href="<?php echo $refLink; ?>">
+              <div class="button-border se-review-button">
+
+                <svg version="1.1" id="Referenten" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="50%" y="50%"
+                   viewBox="0 0 148.7 148.7" style="enable-background:new 0 0 148.7 148.7;" xml:space="preserve">
+                <style type="text/css">
+                  .icon-in{fill:#C3B940;}
+                </style>
+                <g>
+                  <path class="icon-in" d="M91.4,72.9H88c9.2-4.6,15.6-14.2,15.6-25.1c0-15.5-12.6-28.1-28.1-28.1S47.3,32.2,47.3,47.7
+                    c0,11,6.3,20.5,15.6,25.1h-5.6c-0.2,0-21.9,0.3-21.9,21.9v34.1h2.7V94.8c0-18.9,18.4-19.2,19.2-19.2h34.1
+                    c18.9,0,19.2,18.4,19.2,19.2v34.1h2.7V94.8C113.3,94.6,113,72.9,91.4,72.9z M50,47.7c0-14,11.4-25.4,25.4-25.4
+                    c14,0,25.4,11.4,25.4,25.4c0,12.8-9.5,23.4-21.7,25.1h-7.4C59.5,71.1,50,60.5,50,47.7z"/>
+                </g>
+                </svg>
+              </div>
+            </a>
+            <h5><?php echo __('REFERENTEN', 'SimplEvent'); ?></h5>
+            <h6>Schauen Sie alle nochmals alle Referate in Ruhe durch</h6>
+          </div>
+
+
+          <!-- Medien -->
+          <?php
+          if (!wp_is_mobile()) {
+            echo $RevMediaButton;
+          }
+          ?>
+
+
+          <!-- Fotos -->
           <div class="se-review-col" style="text-align:center;">
             <div class="button-border se-review-button se-review-gallery-btn" pagid="<?php echo get_the_ID(); ?>" galid="<?php echo $rCount; ?>">
               <svg version="1.1" id="Fotos" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="50%" y="50%"
@@ -49,30 +111,20 @@ if( have_rows('reviews') ) {
               </g>
               </svg>
             </div>
-            <h5>FOTOS</h5>
+            <h5><?php echo __('FOTOS', 'SimplEvent'); ?></h5>
             <h6>Impressionen rund um die Bühne</h6>
           </div>
-          <div class="se-review-col" style="text-align:center;">
-            <a href="<?php echo $refLink; ?>">
-              <div class="button-border se-review-button">
 
-                <svg version="1.1" id="Referenten" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="50%" y="50%"
-                   viewBox="0 0 148.7 148.7" style="enable-background:new 0 0 148.7 148.7;" xml:space="preserve">
-                <style type="text/css">
-                  .icon-in{fill:#C3B940;}
-                </style>
-                <g>
-                  <path class="icon-in" d="M91.4,72.9H88c9.2-4.6,15.6-14.2,15.6-25.1c0-15.5-12.6-28.1-28.1-28.1S47.3,32.2,47.3,47.7
-                    c0,11,6.3,20.5,15.6,25.1h-5.6c-0.2,0-21.9,0.3-21.9,21.9v34.1h2.7V94.8c0-18.9,18.4-19.2,19.2-19.2h34.1
-                    c18.9,0,19.2,18.4,19.2,19.2v34.1h2.7V94.8C113.3,94.6,113,72.9,91.4,72.9z M50,47.7c0-14,11.4-25.4,25.4-25.4
-                    c14,0,25.4,11.4,25.4,25.4c0,12.8-9.5,23.4-21.7,25.1h-7.4C59.5,71.1,50,60.5,50,47.7z"/>
-                </g>
-                </svg>
-              </div>
-            </a>
-            <h5>REFERENTEN</h5>
-            <h6>Schauen Sie alle nochmals alle Referate in Ruhe durch</h6>
-          </div>
+
+          <!-- Medien -->
+          <?php
+          if (wp_is_mobile()) {
+            echo $RevMediaButton;
+          }
+          ?>
+
+
+          <!-- Inhalt Medien -->
           <div class="se-review-medien-container" style="display:none;">
             <?php
             $mm = get_sub_field('medienmitteilungen');
@@ -93,9 +145,7 @@ if( have_rows('reviews') ) {
             }
             ?>
           </div>
-          <div class="se-review-medien-line" style="border-top:2px solid <?php echo esc_attr( get_option( 'main_color_picker' ) ) ; ?>;">
 
-          <h6 class="se-review-medien-button" mopen="N" mid="<?php echo $rCount; ?>" style="text-align:center;  " class="se-mc-txt">MEDIEN</h6>
 
           </div>
 
@@ -157,7 +207,7 @@ jQuery(document).ready(function($){
 
   var mediaBtn = $('.se-review-medien-button');
   var mediaBars = $('.se-review-media-bar');
-  TweenMax.set(mediaBars, {autoAlpha: 0, y: '20px'})
+  TweenMax.set(mediaBars, {autoAlpha: 0, y: '20px'});
   mediaBtn.on('click', function(){
     let mID = $(this).attr('mid');
     let tMedia = $('.se-review-strip[rid="'+mID+'"]').find('.se-review-medien-container');

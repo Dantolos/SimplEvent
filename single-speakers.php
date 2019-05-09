b1e9bb5d30558967942f20cd35bb6e865d74155b
Dantolos <aaron.giaimo@gmail.com>
2019-05-09 14:08:24 +0200

    1.0.58
    - Language Partner if no WPML

--- single-speakers.php
+++ single-speakers.php
@@ -11,8 +11,13 @@ $SpslugFull = explode('/', $current_url);
 $SpslugFull = array_filter($SpslugFull);
 
 $Spslug = $SpslugFull[count($SpslugFull)];
+if ( function_exists('icl_object_id') ) {
+  $url = home_url() . '/' . ICL_LANGUAGE_CODE . '/speaker/?j='.$term->name.'&r=' . $Spslug;
+}else{
+  $url = home_url() . '/speaker/?j='.$term->name.'&r=' . $Spslug;
+}
 
-$url = home_url() . '/' . ICL_LANGUAGE_CODE . '/speaker/?j='.$term->name.'&r=' . $Spslug;
+
 
 wp_redirect( $url );
 exit;

22e05afe6cce77584c3407993f048bf1a4240a67
Dantolos <aaron.giaimo@gmail.com>
2019-09-19 14:32:52 +0200

    1.1.62
    - svgMap

--- template-maps.php
+++ template-maps.php
@@ -5,9 +5,26 @@
 get_header();
 ?>
 
-<div class="se-strip" style="padding:0; float:left; width:100%; height:100%;">
+<div class="se-strip se-map-container">
   <?php echo the_content(); ?>
 </div>
 
+<script type="text/javascript">
+jQuery(document).ready(function($){
+  var MapContainer = $('.se-map-container');
+
+  FitMapContainer();
+  MapContainer.on('resize', function() {
+    FitMapContainer();
+  });
+
+  function FitMapContainer(){
+    let mapHeight = $(window).innerHeight() - $('.se-navbar-container').innerHeight();
+    MapContainer.css({'height': mapHeight });
+    console.log(mapHeight);
+  }
+
+});
+</script>
 <?php
 get_footer();

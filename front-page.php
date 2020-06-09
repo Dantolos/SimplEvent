74f2e4dc953f9f9d14de12906720a093da1a63b6
Dantolos <45291569+Dantolos@users.noreply.github.com>
2020-06-09 10:52:05 +0200

    1.1.98

--- front-page.php
+++ front-page.php
@@ -345,8 +345,8 @@ if( get_field('strip', $curPageID)  ) {
             sliderLogo.css({'padding': logosize });
             sliderLogo.find('img').attr('src', SlideArray[currentSlide]['logo']);
             sliderLogo.fadeIn();
-            sliderTitel.append(SlideArray[currentSlide]['titel']);
-            sliderDatum.append(SlideArray[currentSlide]['datum']);
+            sliderTitel.innerHTML(SlideArray[currentSlide]['titel']);
+            sliderDatum.innerHTML(SlideArray[currentSlide]['datum']);
 
 
             currentSlide++;

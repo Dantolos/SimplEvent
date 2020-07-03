d409c631f2b00ce3b065763199ba2dac1acf3540
Dantolos <45291569+Dantolos@users.noreply.github.com>
2020-07-03 17:35:23 +0200

    1.2.23

--- header.php
+++ header.php
@@ -53,6 +53,7 @@
     }
 
     /*------------------------------Send Global Variables---------------------------*/
+    $language = (ICL_LANGUAGE_CODE) ? ICL_LANGUAGE_CODE : 'de';
     $wnm_custom = array( 'templateUrl' => get_template_directory_uri(), 'lang' => ICL_LANGUAGE_CODE );
     $scriptToAdGlobal = array('script-js', 'restapi-js',  );
     foreach( $scriptToAdGlobal as $script ){
